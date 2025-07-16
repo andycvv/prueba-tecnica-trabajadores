84eb9d3edbedda43f06ef4d426ba61de4dd8b7e7
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-07-16 12:43:15 -0500

    agregar DTO para Distrito

---
+++ backend-myper/backend-myper/DTOs/DistritoDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend_myper.DTOs
+{
+    public class DistritoDTO
+    {
+        public int Id { get; set; }
+        public required string NombreDistrito { get; set; }
+    }
+}
