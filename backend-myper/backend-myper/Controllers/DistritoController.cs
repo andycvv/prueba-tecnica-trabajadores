b26f717cf4aee368c273f4b9982ce39f34788956
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-07-16 12:43:25 -0500

    agregar controlador y endpoint para listar todos los distritos

---
+++ backend-myper/backend-myper/Controllers/DistritoController.cs
@@ -0,0 +1,31 @@
+﻿using backend_myper.Data;
+using backend_myper.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend_myper.Controllers
+{
+    [ApiController]
+    [Route("api/distritos")]
+    public class DistritoController
+    {
+        private readonly AppDbContext _context;
+        public DistritoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<List<DistritoDTO>> Get()
+        {
+            var distritos = await _context.Distritos
+                .Select(d => new DistritoDTO
+                {
+                    Id = d.Id,
+                    NombreDistrito = d.NombreDistrito
+                })
+                .ToListAsync();
+            return distritos;
+        }
+    }
+}
