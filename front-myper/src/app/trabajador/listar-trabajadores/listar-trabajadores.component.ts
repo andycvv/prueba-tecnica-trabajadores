805323e61231a7e6c497008b822c86c53f7c2706
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-07-16 13:43:54 -0500

    agregar modal con el formulario al listado de trabajadores y también un filtro por sexo

--- front-myper/src/app/trabajador/listar-trabajadores/listar-trabajadores.component.ts
+++ front-myper/src/app/trabajador/listar-trabajadores/listar-trabajadores.component.ts
@@ -1,12 +1,17 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { TrabajadorDTO } from '../trabajador.interface';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
+import { TrabajadorCreacionDTO, TrabajadorDTO, TrabajadorEditarDTO } from '../trabajador.interface';
 import { TrabajadorService } from '../trabajador.service';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ModalFormularioComponent } from '../modal-formulario/modal-formulario.component';
+import { Modal } from 'bootstrap';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-listar-trabajadores',
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ModalFormularioComponent,
+    FormsModule
   ],
   templateUrl: './listar-trabajadores.component.html',
   styleUrl: './listar-trabajadores.component.css'
@@ -17,10 +22,82 @@ export class ListarTrabajadoresComponent implements OnInit {
   trabajadores: TrabajadorDTO[] = []
   trabajadoresFiltrados: TrabajadorDTO[] = []
 
+  filtroSexo: '' | 'M' | 'F' = '';
+
+  modoFormulario: 'crear' | 'editar' = 'crear';
+  trabajadorSeleccionado: TrabajadorEditarDTO | null = null;
+
+  @ViewChild(ModalFormularioComponent)
+  modalFormularioComponent!: ModalFormularioComponent;
+
   ngOnInit(): void {
+    this.listarTrabajadores();
+  }
+
+  listarTrabajadores() {
     this.trabajadorService.getAll().subscribe(trabajadores => {
       this.trabajadores = trabajadores;
       this.trabajadoresFiltrados = trabajadores;
     })
   }
+  
+  filtrarTrabajadores() {
+    if (!this.filtroSexo) {
+      this.trabajadoresFiltrados = [...this.trabajadores];
+    } else {
+      this.trabajadoresFiltrados = this.trabajadores.filter(t => t.sexo === this.filtroSexo);
+    }
+  }
+
+  eliminar(id: number) {
+    Swal.fire({
+      title: "¿Está seguro de eliminar el registro?",
+      text: "Esta acción no se puede revertir",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sí, eliminar"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.trabajadorService.delete(id).subscribe(() => {
+          this.listarTrabajadores();
+          Swal.fire({
+            title: "Eliminado!",
+            text: "El trabajador ha sido eliminado.",
+            icon: "success"
+          });
+        })
+      }
+    });
+  }
+
+  abrirModalCrear() {
+    this.modoFormulario = 'crear';
+    this.trabajadorSeleccionado = null;
+    this.modalFormularioComponent.resetFormulario();
+
+    const modal = document.getElementById('exampleModal')!;
+    const instance = Modal.getOrCreateInstance(modal);
+    instance.show();
+  }
+
+  abrirModalEditar(id: number) {
+    this.modoFormulario = 'editar';
+    this.trabajadorService.getById(id).subscribe(trabajador => {
+      this.trabajadorSeleccionado = trabajador;
+
+      const modal = document.getElementById('exampleModal')!;
+      const instance = Modal.getOrCreateInstance(modal);
+      instance.show();
+    });
+  }
+
+  actualizarLista() {
+    const modal = document.getElementById('exampleModal')!;
+    const instance = Modal.getOrCreateInstance(modal);
+    instance.hide();
+    this.listarTrabajadores();
+  }
+
 }
