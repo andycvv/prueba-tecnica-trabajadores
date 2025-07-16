cf03e50a25cafd5e4bb85b1137fd02dbff2cef53
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-07-16 09:48:14 -0500

    crear controlador para trabajadores

---
+++ backend-myper/backend-myper/Controllers/TrabajadorController.cs
@@ -0,0 +1,17 @@
+﻿using backend_myper.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_myper.Controllers
+{
+    [Route("api/trabajadores")]
+    [ApiController]
+    public class TrabajadorController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public TrabajadorController(AppDbContext _context)
+        {
+            this._context = _context;
+        }
+    }
+}
