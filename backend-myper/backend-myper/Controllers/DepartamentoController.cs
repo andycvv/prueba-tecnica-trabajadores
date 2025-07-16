4bcf7010e15f50cfb826f649aae9ed2737110280
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-07-16 12:42:01 -0500

    crear controlador y endpoint para obtener todos los departamentos

---
+++ backend-myper/backend-myper/Controllers/DepartamentoController.cs
@@ -0,0 +1,30 @@
+﻿using backend_myper.Data;
+using backend_myper.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend_myper.Controllers
+{
+    [ApiController]
+    [Route("api/departamentos")]
+    public class DepartamentoController
+    {
+        private readonly AppDbContext _context;
+        public DepartamentoController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<List<DepartamentoDTO>> Get()
+        {
+            var departamentos = await _context.Departamentos
+                .Select(d => new DepartamentoDTO
+                {
+                    Id = d.Id,
+                    NombreDepartamento = d.NombreDepartamento
+                })
+                .ToListAsync();
+            return departamentos;
+        }
+    }
+}
