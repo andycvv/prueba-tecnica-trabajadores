59fde4c11c4009e619ad85817d1d96840ca5868d
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-07-16 12:42:29 -0500

    agregar DTO para Provincia

---
+++ backend-myper/backend-myper/DTOs/ProvinciaDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend_myper.DTOs
+{
+    public class ProvinciaDTO
+    {
+        public int Id { get; set; }
+        public required string NombreProvincia { get; set; }
+    }
+}
