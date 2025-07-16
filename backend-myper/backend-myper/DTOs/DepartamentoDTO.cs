bf434a92758042897819a279ba1237196f65700b
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-07-16 12:41:43 -0500

    crear DTO para Departamento

---
+++ backend-myper/backend-myper/DTOs/DepartamentoDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace backend_myper.DTOs
+{
+    public class DepartamentoDTO
+    {
+        public int Id { get; set; }
+        public required string NombreDepartamento { get; set; }
+    }
+}
