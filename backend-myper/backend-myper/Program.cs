6a4b384f6dbe801f215a5a705a19c304b98d15aa
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-07-16 09:47:16 -0500

    agregar swagger para testear

--- backend-myper/backend-myper/Program.cs
+++ backend-myper/backend-myper/Program.cs
@@ -7,6 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<AppDbContext>(app =>
 {
     app.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -17,7 +20,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
