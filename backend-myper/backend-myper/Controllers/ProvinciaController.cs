825e9bb65d2893c686f8284fb4b7942cc6f2f65a
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-07-16 12:43:08 -0500

    agregar controlador y endpoint para listar todas las provincias

---
+++ backend-myper/backend-myper/Controllers/ProvinciaController.cs
@@ -0,0 +1,30 @@
+﻿using backend_myper.Data;
+using backend_myper.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend_myper.Controllers
+{
+    [ApiController]
+    [Route("api/provincias")]
+    public class ProvinciaController
+    {
+        private readonly AppDbContext _context;
+        public ProvinciaController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<List<ProvinciaDTO>> Get()
+        {
+            var provincias = await _context.Provincias
+                .Select(p => new ProvinciaDTO
+                {
+                    Id = p.Id,
+                    NombreProvincia = p.NombreProvincia
+                })
+                .ToListAsync();
+            return provincias;
+        }
+    }
+}
