9543fe9c1a4d6d4eee2351acc144259cd174cc03
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-07-16 09:46:15 -0500

    conectar a base de datos local sql server

--- backend-myper/backend-myper/Program.cs
+++ backend-myper/backend-myper/Program.cs
@@ -1,10 +1,16 @@
+using backend_myper.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddDbContext<AppDbContext>(app =>
+{
+    app.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
