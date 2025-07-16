60f634c051489242a1f6b7a20e348f6183a4ceba
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-07-16 09:49:56 -0500

    agregar DTOs para la entidad trabajador

---
+++ backend-myper/backend-myper/DTOs/TrabajadorCreacionDTO.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend_myper.DTOs
+{
+    public class TrabajadorCreacionDTO
+    {
+        [Required(ErrorMessage = "El campo es requerido")]
+        [StringLength(3, ErrorMessage = "El campo no debe exceder los 3 caracteres")]
+        public required string TipoDocumento { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [StringLength(50, ErrorMessage = "El campo no debe exceder los 50 caracteres")]
+        public required string NumeroDocumento { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [StringLength(500, ErrorMessage = "El campo no debe exceder los 500 caracteres")]
+        public required string Nombres { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [StringLength(1, ErrorMessage = "El campo no debe exceder los 1 caracteres")]
+        public required string Sexo { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        public int DepartamentoId { get; set; }
+        [Required(ErrorMessage = "El campo es requerido")]
+        public int ProvinciaId { get; set; }
+        [Required(ErrorMessage = "El campo es requerido")]
+        public int DistritoId { get; set; }
+    }
+}
---
+++ backend-myper/backend-myper/DTOs/TrabajadorDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace backend_myper.DTOs
+{
+    public class TrabajadorDTO
+    {
+        public int Id { get; set; }
+        public required string TipoDocumento { get; set; }
+        public required string NumeroDocumento { get; set; }
+        public required string Nombres { get; set; }
+        public required string Sexo { get; set; }
+        public required string Departamento { get; set; }
+        public required string Provincia { get; set; }
+        public required string Distrito { get; set; }
+    }
+}
